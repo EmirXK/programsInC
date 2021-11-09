3ca2c706954ef8f973ceaa73183642e758df6ed1
EmirXK <92262991+EmirXK@users.noreply.github.com>
2021-11-09 10:38:35 +0300

    Add files via upload

---
+++ game.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int guess, counter, randNum, win;
+	counter = 0;
+	win = 0;
+	srand(time(NULL));
+	randNum = rand() % 101;
+	printf("Welcome to the game, you have 6 attempts to guess a random number between 0 and 100. Good luck.\n");
+	while(counter<6){
+		scanf("%d", &guess);
+		if(guess == randNum){
+			printf("You Win!\n");
+			win = 1;
+			counter = 7;
+		} else if(guess>randNum){
+			printf("Input a lower number\n");
+			counter = counter + 1;
+		} else{
+			printf("Input a higher number\n");
+			counter = counter + 1;
+		}
+	} if(win==0){
+		printf("You lost.. The random number was: %d \n", randNum);
+	} else{}
+	return 0;
+}
+
