62141c26b0db3b8b5e87971c9af75fe935a817ae
EmirXK <92262991+EmirXK@users.noreply.github.com>
2021-11-28 23:40:19 +0300

    Add files via upload

---
+++ hi_evolvedi.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+	int i,j,k,l,m,n,o;
+	for(i=1;i<=5;i++){
+		printf("H");
+		for(j=1;j<=7;j++){
+		printf(" ");
+		}
+		printf("H");
+		for(n=1;n<=6;n++){
+			printf(" ");
+		}
+		printf("I");
+		printf("\n");
+	} 
+	
+	for(k=1;k<=5;k++){
+		printf("H ");
+	} 
+	printf("     ");
+	printf("I");
+	printf("\n");
+	
+	for(l=1;l<=5;l++){
+		printf("H");
+		for(m=1;m<=7;m++){
+		printf(" ");
+		}
+		printf("H");
+		for(o=1;o<=6;o++){
+			printf(" ");
+		}
+		printf("I");
+		 printf("\n");
+	}
+	
+}
